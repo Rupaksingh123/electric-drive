0fe1ec49840afbadc1a18e912d6f9acdb7f5dc3d
Rupaksingh123 <rupaksingh12321@gmail.com>
2025-06-12 08:53:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import RateInput from './RateInput'; // ⬅️ Import your new component
 
 function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     {/* <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header> */}
+  //   </div>
+
+    
+  // );
 
   
+   return (
+    <table>
+      <tbody>
+        <RateInput /> {/* ⬅️ Use your custom component */}
+      </tbody>
+    </table>
+  );
 }
 
 export default App;
